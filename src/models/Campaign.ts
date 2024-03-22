4e451e05e23bd72aabba48c2a71d409c624dd7e2
Eric <ericfrancis1@proton.me>
2024-03-21 22:02:31 -0500

    Campaign model and typings

--- src/models/Campaign.ts
+++ src/models/Campaign.ts
@@ -1,10 +1,77 @@
 import mongoose from 'mongoose';
+import { ESourceType, EContentType, EFilterComponentType, EPublisherType } from '../typings/index';
 
 const campaignSchema = new mongoose.Schema({
     name: {
         type: String,
         require: [true, 'Campaign must have a name']
-    }
+    },
+    disabled: {
+        type: Boolean,
+        default: () => false
+    },
+    downloadHistory: [String],
+    source: {
+        type: {
+            type: String,
+            enum: Object.values(ESourceType),
+            require: [true, 'Campaign must have a source type']
+        },
+        contentType: {
+            type: String,
+            enum: Object.values(EContentType),
+            require: [true, 'Campaign must have a content type']
+        },
+        externalId: {
+            type: String,
+            require: [true, 'Campaign must have an external id']
+        }
+    },
+    filters: [
+        {
+            name: {
+                type: String,
+                require: [true, 'Filter must have a name']
+            },
+            base: {
+                type: {
+                    type: String,
+                    enum: Object.values(EFilterComponentType),
+                    require: [true, 'Filter base must have a type']
+                },
+                content_id: {
+                    type: String
+                },
+                filterIndex: {
+                    type: Number
+                }
+            },
+            ingredient: {
+                type: {
+                    type: String,
+                    enum: Object.values(EFilterComponentType),
+                    require: [true, 'Filter ingredient must have a type']
+                },
+                content_id: String,
+                filterIndex: Number
+            },
+            options: {}
+        }
+    ],
+    publishTo: [
+        {
+            type: {
+                type: String,
+                enum: Object.values(EPublisherType),
+                require: [true, 'Publisher must have a type']
+            },
+            externalId: {
+                type: String,
+                require: [true, 'Publisher must have an external id']
+            },
+            disabled: Boolean
+        }
+    ]
 });
 
 const Campaign = mongoose.model('Campaign', campaignSchema);
---
+++ src/typings/index.ts
@@ -0,0 +1,25 @@
+
+
+export enum ESourceType {
+    INSTAGRAM = 'INSTAGRAM',
+    TIKTOK = 'TIKTOK',
+    YOUTUBE = 'YOUTUBE'
+};
+
+export enum EContentType {
+    IMAGE = 'IMAGE',
+    VIDEO = 'VIDEO'
+};
+
+export enum EFilterComponentType {
+    SOURCE = 'SOURCE',
+    SAVED = 'SAVED',
+    TEMP = 'TEMP'
+};
+
+export enum EPublisherType {
+    TIKTOK_ACCOUNT = 'TIKTOK_ACCOUNT',
+    INSTAGRAM_ACCOUNT = 'INSTAGRAM_ACCOUNT',
+    TELEGRAM_CHANNEL = 'TELEGRAM_CHANNEL',
+    DOWNLOAD_TO_MACHINE = 'DOWNLOAD_TO_MACHINE'
+};
