d87f2668f4d829dd9c95da82194b178db20a1e16
Eric <ericfrancis1@proton.me>
2024-03-23 14:13:53 -0500

    4 spaces ui

--- services/ui/src/App.tsx
+++ services/ui/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className='text-red-400'>
-      <h1>Hello from Typescript!!!!</h1>
-    </div>
-  );
+    return (
+        <div className='text-red-400'>
+            <h1>Hello from Typescript!!!!</h1>
+        </div>
+    );
 }
 
 export default App;
