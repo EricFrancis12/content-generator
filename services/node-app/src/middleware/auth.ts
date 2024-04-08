3e9bf9ed28a6e0c6307acc38d3fc1878eddb0c4c
Eric <ericfrancis1@proton.me>
2024-04-07 20:48:01 -0500

    changed async to regular function

--- services/node-app/src/middleware/auth.ts
+++ services/node-app/src/middleware/auth.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import config from '../config/config';
 const { AUTH_TOKEN } = config;
 
-export async function auth(req: Request, res: Response, next: NextFunction) {
+export function auth(req: Request, res: Response, next: NextFunction) {
     if (AUTH_TOKEN != null) {
         const authToken = req.headers.authorization?.split(' ').pop();
         if (authToken !== AUTH_TOKEN) {
