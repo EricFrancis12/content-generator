26b027148f37f5707a64a574eec8d316c9ba52f1
Eric <ericfrancis1@proton.me>
2024-04-07 20:55:14 -0500

    NODE_ENV

--- services/node-app/src/middleware/auth.ts
+++ services/node-app/src/middleware/auth.ts
@@ -3,7 +3,7 @@ import config from '../config/config';
 const { AUTH_TOKEN } = config;
 
 export function auth(req: Request, res: Response, next: NextFunction) {
-    if (AUTH_TOKEN != null) {
+    if (process.env.NODE_ENV !== 'development') {
         const authToken = req.headers.authorization?.split(' ').pop();
         if (authToken !== AUTH_TOKEN) {
             return res.json({
