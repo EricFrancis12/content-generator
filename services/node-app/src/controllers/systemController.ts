b021f4eb8f3d024840f063d16deb648d427a8f06
Eric <ericfrancis1@proton.me>
2024-05-04 21:46:51 -0500

    added diskspace api route

--- services/node-app/src/controllers/systemController.ts
+++ services/node-app/src/controllers/systemController.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from 'express';
-import checkDiskSpace from 'check-disk-space'
+import os from 'os';
+import checkDiskSpace, { DiskSpace } from 'check-disk-space';
 
 export async function getDiskSpace(req: Request, res: Response) {
     try {
-        const result = await checkDiskSpace('/');
+        let diskSpace: DiskSpace;
+        const platform = os.platform();
+        if (platform === 'win32') {
+            diskSpace = await checkDiskSpace('C:\\');
+        } else {
+            diskSpace = await checkDiskSpace('/');
+        }
         res.status(200).json({
             success: true,
-            data: result
+            data: {
+                diskSpace,
+                platform
+            }
         });
     } catch (err) {
         res.status(500).json({
