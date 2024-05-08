18b593c51b6242158529fd3cbf75089fdd84f12b
Eric Francis <134184681+EricFrancis12@users.noreply.github.com>
2024-05-07 20:07:38 -0500

    Delete services/ui/src/App/pages/campaigns directory
    The directory: "services/ui/src/App/pages/campaigns" should be "services/ui/src/App/pages/Campaigns". It appears git is tracking this incorrectly. Deleting this directory now, and will re-commit it correctly shortly.

--- services/ui/src/App/pages/campaigns/ViewCampaign/History/NoHistory.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function NoHistory() {
-    return (
-        <div className='text-center w-full'>No history...</div>
-    )
-}
--- services/ui/src/App/pages/campaigns/ViewCampaign/typings.ts
+++
@@ -1,9 +0,0 @@
-import { IHistoryItem, EOutputType } from '../../../../_shared';
-
-export type TMode = 'list' | 'graph';
-
-export interface IData extends IHistoryItem {
-    date: string,
-    amt: number,
-    outputType?: EOutputType
-}
